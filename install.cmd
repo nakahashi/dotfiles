f325d0831b6bf5825f693168ff04161f7a5e86fd
nakahashi <nakahashi@users.noreply.github.com>
2016-02-14 00:49:01 +0900

    add .editorconfig

--- install.cmd
+++ install.cmd
@@ -1,3 +1,4 @@
 mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
 mklink %HOMEPATH%"\.gvimrc" %HOMEPATH%"\dotfiles\.gvimrc"
 mklink %HOMEPATH%"\.gitconfig" %HOMEPATH%"\dotfiles\.gitconfig"
+mklink %HOMEPATH%"\.editorconfig" %HOMEPATH%"\dotfiles\.editorconfig"
--- install.sh
+++ install.sh
@@ -1,1 +1,7 @@
-#!/bin/shln -sf ~/dotfiles/.vimrc ~/.vimrcln -sf ~/dotfiles/.gvimrc ~/.gvimrcln -sf ~/dotfiles/.bashrc ~/.bashrcln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+#!/bin/sh
+
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
+ln -sf ~/dotfiles/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.editorconfig ~/.editorconfig
