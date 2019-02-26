a204ce62435ba84e1fdd1d942d018161f29e4d0e
nakahashi kentaro <nakahashi_k@uuum.jp>
2019-02-26 09:34:59 +0900

    update

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gvimrc ~/.gvimrc
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.editorconfig ~/.editorconfig
 ln -sf ~/dotfiles/vim/commands.md ~/.vim-commands.md
 cp ~/dotfiles/.zshenv ~/.zshenv
