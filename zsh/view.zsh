c1377cc710e71ab11ae60f628b781a8e08d85efc
nakahashi <nakahashi@users.noreply.github.com>
2026-03-27 22:25:38 +0900

    改行でバグるのを修正

--- zsh/view.zsh
+++ zsh/view.zsh
@@ -20,15 +20,20 @@ function vcs_echo {
   local st branch color
   STY= LANG=en_US.UTF-8 vcs_info
   st=`git status 2> /dev/null`
-  if [[ -z "$st" ]]; then return; fi
+  if [[ -z "$st" ]]; then
+    PROMPT_VCS=""
+    return
+  fi
   branch="$vcs_info_msg_0_"
   if   [[ -n "$vcs_info_msg_1_" ]]; then color=${fg[green]} #staged
   elif [[ -n "$vcs_info_msg_2_" ]]; then color=${fg[red]} #unstaged
   elif [[ -n `echo "$st" | grep "^Untracked"` ]]; then color=${fg[blue]} # untracked
   else color=${fg[cyan]}
   fi
-  echo "%{$color%}(%{$branch%})%{$reset_color%}" | sed -e s/@/"%F{yellow}@%f%{$color%}"/
+  PROMPT_VCS="%{$color%}(%{$branch%})%{$reset_color%}"
+  PROMPT_VCS=$(echo "$PROMPT_VCS" | sed -e s/@/"%F{yellow}@%f%{$color%}"/)
 }
-PROMPT='%F{yellow}[%~]%f `vcs_echo`%(?.$.%F{red}$%f) '
+precmd() { vcs_echo }
+PROMPT='%F{yellow}[%~]%f ${PROMPT_VCS}%(?.$.%F{red}$%f) '
 setopt transient_rprompt
 
